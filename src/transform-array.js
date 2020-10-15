242b5a90ed55bfb451206e49217f44ae1034b988
ku13shov <kuleshov000@gmail.com>
2020-10-15 22:48:45 +0300

    feat: add new solution for transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,43 +1,21 @@
 
 
 module.exports = function transform(arr) {
-  
-  let copyArr = arr.slice();
   let newArr = [];
-  
-  for (let i = 0; i < copyArr.length; i++) {
-    if ((copyArr[i] === '--double-next') && (i !== copyArr.length-1)) {
-      newArr.push(copyArr[i + 1]);
-      copyArr.splice(i, 1);
-    }
-    if (copyArr[i] === '--double-prev') {
-      newArr.push(copyArr[i - 1]);
-      copyArr.splice(i, 1);
-    }
-    if (copyArr[i] === '--discard-next') {
-      copyArr.splice(i, 2);
-    }
-    if ((copyArr[i] === '--discard-prev') && (i === copyArr.length-1)) {
-      newArr.splice((i - 2), 1);
-      copyArr.splice(i, 1);
-    } else if ((copyArr[i] === '--discard-prev')) {
-      newArr.splice((i - 1), 1);
-      copyArr.splice(i, 1);
-    }
-    newArr.push(copyArr[i]);
-  }
-  for (let i = 0; i < newArr.length; i++) {
-    if ((newArr[i] === '--double-next') && (i === newArr.length-1)) {
-      newArr.splice(i, 1);
-    }
-    if ((newArr[i] === undefined) && (i === newArr.length-1)) {
-      newArr.splice(i, 1);
-    }
-    if ((newArr[i] === '--discard-next') && (i === newArr.length-1)) {
-      newArr.splice(i, 1);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      newArr.push(arr[i + 1]);
+      newArr.pop();
+      i += 2;
+    } else if (arr[i] === '--discard-prev') {
+      newArr.pop();
+    } else if (arr[i] === '--double-next') {
+      newArr.push(arr[i + 1]);
+    } else if (arr[i] === '--double-prev') {
+      newArr.push(arr[i - 1]);
+    } else {
+      newArr.push(arr[i]);
     }
   }
-  
   return newArr;
-
 };
