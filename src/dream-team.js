43fc0fe936de0dfe0f071f2d1619827769ad3b4e
ku13shov <kuleshov000@gmail.com>
2020-10-17 12:46:37 +0300

    fead: add solution for dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let dreamTeam = [];
+  let k = 0;
+
+  if (Array.isArray(members)) {
+    for (let i = 0; i < members.length; i++) {
+      if (members[i] === null || 
+          members[i] === undefined || 
+          typeof members[i] === 'number' || 
+          typeof members[i] === 'object' || 
+          typeof members[i] === 'boolean') {
+        
+      } else if (members[i][k] == ' ') {
+        for (let j = 0; j < members[i].length; j++) {
+          if (members[i][j] !== ' ') {
+            dreamTeam.push(members[i][j].toUpperCase());
+            break;
+          }
+        }
+        
+      } else {
+        dreamTeam.push(members[i][k].toUpperCase());
+      }
+    }
+    return dreamTeam.sort().join('');
+  } else {
+    return false;
+  }
 };
